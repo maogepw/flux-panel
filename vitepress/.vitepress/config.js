eaf55f74e78383cd63209b18f51ebd26d0680c1a
猫哥 <104515697+maogepw@users.noreply.github.com>
2025-09-05 02:54:28 +0800

    Update config.js

--- vitepress/.vitepress/config.js
+++ vitepress/.vitepress/config.js
@@ -3,17 +3,13 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'flux-panel',
   description: '基于gost的转发面板',
-  
+  base: '/flux-panel/',
   themeConfig: {
     nav: [
       { text: '首页', link: '/' },
       { text: '快速开始', link: '/getting-started' },
       { text: '使用指南', link: '/guide' },
       { text: '常见问题', link: '/faq' }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/StarVM-OpenSource/flux-panel' },
     ]
   }
 })
